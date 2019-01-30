91992ead8e8d31b839c9286837d4411e65f2dd0f
Ali Madni <ali555m@hotmail.com>
2019-01-30 10:11:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {tempahT: "next hype"}
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
-  return Object.assign({}, playlist, {artistName: [songTitle]})
+  return Object.assign(playlist, {artistName: [songTitle]})
 }
